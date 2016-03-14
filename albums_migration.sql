13a1218129ea275c8055bbf58e70c4b4eb2195eb
Margot McMahon <amcmahon@nes.ru>
2016-03-14 14:57:01 -0500

    Finished music table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
  	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
- 	artist CHAR(100) NOT NULL,
  	name CHAR (100) NOT NULL,
+ 	artist CHAR(100) NOT NULL,
  	release_date YEAR(4) NOT NULL,
- 	sales_in_millions FLOAT(10, 2) NOT NULL,
  	genre CHAR(100) NOT NULL,
+ 	sales_in_millions FLOAT(10, 2) NOT NULL,
  	PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+INSERT INTO albums (name, artist, release_date, genre, sales_in_millions)
+VALUES ('The Modern Lovers', 'The Modern Lovers', '1976', 'proto-punk', '1.0'),
+('The Dance of Death & Other Plantation Favorites', 'John Fahey', '1967', 'folk', '0.5'),
+('The Raincoats', 'The Raincoats', '1979', 'post-punk', '1.1'),
+('You Turn Me On', 'Beat Happening', '1992', 'twee pop', '0.4'),
+('Work And Non Work', 'Broadcast', '1997', 'indie electronic', '1.1'),
+('Camoufleur', 'Gastr Del Sol', '1998', 'post rock', '1.1'),
+('Alien Lanes', 'Guided By Voices', '1995', 'lo-fi', '1.1'),
+('Back In Your Life', 'Jonathan Richman & The Modern Lovers', '1979', 'folk', '0.3'),
+('Death of a Ladies'' Man', 'Leonard Cohen', '1977', 'folk', '1.9'),
+('Loveless', 'My Bloody Valentine', '1991', 'shoegaze', '2.0'),
+('The Head On The Door', 'The Cure', '1985', 'gothic rock', '5.1'),
+('Black Candy', 'Beat Happening', '1989', 'post-punk', '0.2'),
+('Life''s a Riot With Spy vs. Spy', 'Billy Bragg', '1983', 'folk punk', '1.2'),
+('Broom', 'Someone Still Loves You Boris Yeltsin', '2005', 'indie pop', '1.0'),
+('Beautiful Rat Sunset', 'The Mountain Goats', '1994', 'lo-fi', '0.1')
