75718d11ed4211e14103cba15348e4ee7485eaf1
Margot McMahon <amcmahon@nes.ru>
2016-03-14 11:59:21 -0500

    finished empty album table!

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
- USE codeup_test_db;
- DROP TABLE IF EXISTS albums;
- CREATE TABLE albums (
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
  	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
- 	artist VARCHAR(100) NOT NULL,
- 	name VARCHAR (100) NOT NULL,
+ 	artist CHAR(100) NOT NULL,
+ 	name CHAR (100) NOT NULL,
  	release_date YEAR(4) NOT NULL,
- 	sales FLOAT,
- 	genre VARCHAR(100) NOT NULL,
+ 	sales_in_millions FLOAT(10, 2) NOT NULL,
+ 	genre CHAR(100) NOT NULL,
  	PRIMARY KEY (id)
- 	);
+);
