1c40d720ea99a8f6cfe662d56616c3376ded5053
Margot McMahon <amcmahon@nes.ru>
2016-03-15 14:46:55 -0500

    where continued. -_- /

--- where_exercises.sql
+++ where_exercises.sql
@@ -56,3 +56,30 @@ ORDER BY birth_date DESC;
 SELECT first_name, last_name
 FROM employees
 ORDER BY length(first_name) DESC, length(last_name) DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE (first_name = 'Vidya' OR
+   first_name = 'Maya'
+   OR first_name = 'Irena')
+AND
+(gender = "M");
+
+/*E last names*/
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE `hire_date` LIKE '199%' AND `birth_date` LIKE '%12-25';
+
+SELECT first_name, last_name
+FROM employees
+WHERE `last_name` LIKE '%q%' AND `last_name` NOT LIKE '%qu%';
+
+
