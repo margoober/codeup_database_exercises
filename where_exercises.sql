d62252e9d4654cf265ab08daea47407b6a4d88b4
Margot McMahon <amcmahon@nes.ru>
2016-03-15 14:25:04 -0500

    finished exercises IN CURLZ MT -- feat. CURLZ GURLZ

--- where_exercises.sql
+++ where_exercises.sql
@@ -20,3 +20,39 @@ OR last_name = 'Baek';
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+
+/*Irena, Vidya, Maya*/
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+/*E last names*/
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE "E%";
+
+/*born in the 90s*/
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN "1990-01-01" AND "1999-12-31";
+
+/*born on Christmas*/
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE birth_date LIKE "%12-25";
+
+/*last names contain a Q*/
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+/*women born on my birthday!*/
+SELECT first_name, last_name, birth_date, gender
+FROM employees
+WHERE birth_date LIKE '%10-14'
+AND gender = 'F'
+ORDER BY birth_date DESC;
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY length(first_name) DESC, length(last_name) DESC;
