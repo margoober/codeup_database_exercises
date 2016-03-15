cbb37f2e09a2ae6d3e3a4afca62e08eee4290e57
Margot McMahon <amcmahon@nes.ru>
2016-03-15 09:17:14 -0500

    morning seeder

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -16,4 +16,9 @@ VALUES ('The Modern Lovers', 'The Modern Lovers', '1976', 'proto-punk', '1.0'),
 ('Life''s a Riot With Spy vs. Spy', 'Billy Bragg', '1983', 'folk punk', '1.2'),
 ('Broom', 'Someone Still Loves You Boris Yeltsin', '2005', 'indie pop', '1.0'),
 ('Beautiful Rat Sunset', 'The Mountain Goats', '1994', 'lo-fi', '0.1'),
-('Haha Sound', 'Broadcast', '2003', 'indie electronic', '0.1')
+('Haha Sound', 'Broadcast', '2003', 'indie electronic', '0.1'),
+('The Noise Made By People', 'Broadcast', "2000", 'indie electronic', '1.1'),
+('Tender Buttons', 'Broadcast', "2005", 'indie electronic', '1.1'),
+('The Future Crayon', 'Broadcast', "2006", 'indie electronic', '1.1'),
+('Extended Play Two', 'Broadcast', "2000", 'indie electronic', '1.1'),
+('Blind Joe Death', 'John Fahey', '1959', 'folk', '0.1')
